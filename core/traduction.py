e7d75cdf5ff057dbd460ef318bfcad24667f6795
hello-jdj <172093886+hello-jdj@users.noreply.github.com>
2026-04-20 10:15:14 -0300

    Translation settings backend improvement (#112)
    * Translation Settings Backend-Improvement (#94)
    
    * improve performance for solution of issue #94
    
    * optimise solution for issue #94 in time and memory usage
    
    * fix minor typo
    
    * update bot check translation

--- core/traduction.py
+++ core/traduction.py
@@ -1,12 +1,40 @@
 import json
 from core.debug import Info, Debug
+import os
+import re
 
 class Traduction:
+    langs_path = Info.resource_path('assets/models/lang')
     model = {}
-    
+
+    @staticmethod
+    def get_languages():
+        langs = []
+        for file in os.scandir(Traduction.langs_path):
+            if file.is_file() and file.name[-5:] == '.json' and file.name != "en.json":
+                file_name = file.name[:-5]
+                lang_name = Traduction.get_language_name(file.path)
+                if lang_name is None:
+                    lang_name = 'Unknown language'
+                langs.append((lang_name, file_name))
+        langs.sort(key=lambda pair : pair[1])
+        langs.insert(0, ('English', 'en'))
+        return langs
+
+    @staticmethod
+    def get_language_name(lang_path: str):
+        try:
+            with open(lang_path, encoding='utf-8') as data:
+                lines = '\n'.join(data.readlines(2))
+                match = re.search(r'"language_name"\s*:\s*"(.*?)"\s*,', lines)
+                lang_name = match.group(1)
+                return lang_name
+        except Exception:
+            return None
+
     @staticmethod
     def set_translate_model(lang: str):
-        trad_file = Info.resource_path(f"assets/models/{lang}.json")
+        trad_file = os.path.join(Traduction.langs_path, lang + '.json')
         try:
             with open(trad_file, encoding='utf-8') as data:
                 Traduction.model = json.load(data)
